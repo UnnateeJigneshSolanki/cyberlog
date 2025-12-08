861816349e391e16f682bda239cbd236115a1ed9
UnnateeJigneshSolanki <unnasususu2007@gmail.com>
2025-12-08 21:20:45 +0530

    Updated the main.dart with new odd/even checker app

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,56 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const myfirstapp());
+  runApp(const OddEvenDemo());
 }
-class myfirstapp extends StatelessWidget{
-  const myfirstapp({super.key});
+
+class OddEvenDemo extends StatelessWidget {
+  const OddEvenDemo({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+    String result = "";
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("IF LOST, CONTACT"),
+        body: StatefulBuilder(
+          builder: (context, setState) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: controller, decoration: const InputDecoration(
+                    labelText: "Enter a number",
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 11, 86, 147),    
+                      foregroundColor: Colors.white,  
+                    ),
+                    onPressed: () {
+                      int number = int.parse(controller.text);
+                      if (number % 2 == 0) {
+                        result = "The number $number is Even.";
+                      } else {
+                        result = "The number $number is Odd.";
+                      }
+                  setState(() {});
+                    },
+                    child: const Text("Check"),
+                  ),
+                  Text(
+                    "$result",   
+                    style: const TextStyle(fontSize: 20, color: Color.fromARGB(255, 134, 45, 150),),
+                    
+                  ),
+                ],
+              ),
+            );
+          },
         ),
-        body: const Center(
-          child: Text("Name : Unnatee\nPhone : 9137533261 ", textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 22,
-              color: Color.fromARGB(255, 6, 48, 83),
-            ), ),
-        )
-      )
+      ),
     );
   }
 }
